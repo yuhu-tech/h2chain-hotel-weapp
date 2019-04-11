d2acff53c9be6eae1578b3c2173fc8cd91766c5c
in_vane <in_vane@yuhu.tech>
2019-04-11 09:48:36 +0800

    fix: graphql's client

--- miniprogram_npm/wxapp-graphql/index.js
+++ miniprogram_npm/wxapp-graphql/index.js
@@ -1,25 +1,70 @@
 module.exports = (function() {
-var __MODS__ = {};
-var __DEFINE__ = function(modId, func, req) { var m = { exports: {} }; __MODS__[modId] = { status: 0, func: func, req: req, m: m }; };
-var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = { exports: {} }; __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); if(typeof m.exports === "object") { Object.keys(m.exports).forEach(function(k) { __MODS__[modId].m.exports[k] = m.exports[k]; }); if(m.exports.__esModule) Object.defineProperty(__MODS__[modId].m.exports, "__esModule", { value: true }); } else { __MODS__[modId].m.exports = m.exports; } } return __MODS__[modId].m.exports; };
-var __REQUIRE_WILDCARD__ = function(obj) { if(obj && obj.__esModule) { return obj; } else { var newObj = {}; if(obj != null) { for(var k in obj) { if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k]; } } newObj.default = obj; return newObj; } };
-var __REQUIRE_DEFAULT__ = function(obj) { return obj && obj.__esModule ? obj.default : obj; };
-__DEFINE__(1551702904211, function(require, module, exports) {
-var responseHandler = function(resolve, reject, res, errorHandler) {
-    var retData = res.data.errors ? res.data.errors[0].message : {
-        code: 200
+  var __MODS__ = {};
+  var __DEFINE__ = function(modId, func, req) {
+    var m = {
+      exports: {}
     };
-    if(res.statusCode == 200 && retData.code ==200) {
-        resolve(res.data.data);
+    __MODS__[modId] = {
+      status: 0,
+      func: func,
+      req: req,
+      m: m
+    };
+  };
+  var __REQUIRE__ = function(modId, source) {
+    if (!__MODS__[modId]) return require(source);
+    if (!__MODS__[modId].status) {
+      var m = {
+        exports: {}
+      };
+      __MODS__[modId].status = 1;
+      __MODS__[modId].func(__MODS__[modId].req, m, m.exports);
+      if (typeof m.exports === "object") {
+        Object.keys(m.exports).forEach(function(k) {
+          __MODS__[modId].m.exports[k] = m.exports[k];
+        });
+        if (m.exports.__esModule) Object.defineProperty(__MODS__[modId].m.exports, "__esModule", {
+          value: true
+        });
+      } else {
+        __MODS__[modId].m.exports = m.exports;
+      }
+    }
+    return __MODS__[modId].m.exports;
+  };
+  var __REQUIRE_WILDCARD__ = function(obj) {
+    if (obj && obj.__esModule) {
+      return obj;
     } else {
+      var newObj = {};
+      if (obj != null) {
+        for (var k in obj) {
+          if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k];
+        }
+      }
+      newObj.default = obj;
+      return newObj;
+    }
+  };
+  var __REQUIRE_DEFAULT__ = function(obj) {
+    return obj && obj.__esModule ? obj.default : obj;
+  };
+  __DEFINE__(1554946536254, function(require, module, exports) {
+    var responseHandler = function(resolve, reject, res, errorHandler) {
+      var retData = res.data.errors ? res.data.errors[0].message : {
+        code: 200
+      };
+      if (res.statusCode == 200 && retData.code == 200) {
+        resolve(res.data.data);
+      } else {
         reject(res.data);
         if (errorHandler) {
-            errorHandler(res.data);
+          errorHandler(res.data);
         }
-    }
+      }
     }
 
-    var GraphQL = function (obj, retObj) {
+    var GraphQL = function(obj, retObj) {
 
       if (!obj.url) {
         throw "请提供GraphQL请求URL(.url)"
@@ -35,8 +80,8 @@ var responseHandler = function(resolve, reject, res, errorHandler) {
 
       if (retObj) {
         return {
-          query: function (queryObj) {
-            return new Promise(function (resolve, reject) {
+          query: function(queryObj) {
+            return new Promise(function(resolve, reject) {
               wx.request({
                 url: obj.url,
                 method: 'POST',
@@ -45,15 +90,15 @@ var responseHandler = function(resolve, reject, res, errorHandler) {
                   variables: queryObj.variables
                 }),
                 header: queryObj.header || (typeof obj.header === 'function' ? obj.header() : obj.header),
-                complete: function (res) {
+                complete: function(res) {
                   responseHandler(resolve, reject, res, obj.errorHandler);
                 }
               });
             });
           },
 
-          mutate: function (mutateObj) {
-            return new Promise(function (resolve, reject) {
+          mutate: function(mutateObj) {
+            return new Promise(function(resolve, reject) {
               wx.request({
                 url: obj.url,
                 method: 'POST',
@@ -62,7 +107,7 @@ var responseHandler = function(resolve, reject, res, errorHandler) {
                   variables: mutateObj.variables
                 }),
                 header: mutateObj.header || (typeof obj.header === 'function' ? obj.header() : obj.header),
-                complete: function (res) {
+                complete: function(res) {
                   responseHandler(resolve, reject, res);
                 }
               });
@@ -70,7 +115,7 @@ var responseHandler = function(resolve, reject, res, errorHandler) {
           }
         }
       } else {
-        return function (_obj) {
+        return function(_obj) {
 
           if (!_obj.body) {
             throw "请提供GraphQL请求body"
@@ -93,7 +138,10 @@ var responseHandler = function(resolve, reject, res, errorHandler) {
       GraphQL: GraphQL
     }
 
-  }, function (modId) { var map = {}; return __REQUIRE__(map[modId], modId); })
-  return __REQUIRE__(1552617112607);
+  }, function(modId) {
+    var map = {};
+    return __REQUIRE__(map[modId], modId);
+  })
+  return __REQUIRE__(1554946536254);
 })()
 //# sourceMappingURL=index.js.map
